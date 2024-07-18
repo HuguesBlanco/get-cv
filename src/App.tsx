87feff435ca320c4818de28856b2306090641777
Hugues Blanco <contact@huguesblanco.com>
2024-07-18 17:18:58 +0200

    Add CV download link

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import { PDFDownloadLink } from '@react-pdf/renderer';
 import CvViewer from './CvViewer';
+import MyCvAsPdf from './MyCvAsPdf';
 
 function App(): JSX.Element {
-  return <CvViewer />;
+  return (
+    <>
+      <div>
+        <PDFDownloadLink
+          document={<MyCvAsPdf />}
+          fileName="hugues_blanco_alvarez_cv.pdf"
+        >
+          {({ loading }) => (loading ? 'Loading...' : 'Download CV')}
+        </PDFDownloadLink>
+      </div>
+      <CvViewer />
+    </>
+  );
 }
 
 export default App;
