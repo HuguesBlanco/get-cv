23b244fc7d5353e1994ae4bd0969885a8edf4456
Hugues Blanco <contact@huguesblanco.com>
2024-07-23 17:08:41 +0200

    Improve stucture of CV data

--- src/services/cvServiceTypes.ts
+++ src/services/cvServiceTypes.ts
@@ -1,14 +1,43 @@
+export type PostalAddress = {
+  streetNumber: string;
+  streetName: string;
+  additionalAddressInfo: string;
+  city: string;
+  postalCode: string;
+  country: string;
+};
+
+export type Contact = {
+  email: string;
+  phone: string;
+  postalAddress: PostalAddress;
+};
+
+export type Language = {
+  language: string;
+  level: string;
+};
+
 export type Skill = string;
 
-export type ProfessionalExperience = {
+export type JobPosition = {
   title: string;
   description: string;
   skills: Skill[];
+  startDate: string;
+  endDate: string | null;
 };
 
-export type Education = {
-  degree: string;
-  field: string;
+export type Company = {
+  name: string;
+  description?: string;
+  website?: string;
+  location?: string;
+  jobPositions: JobPosition[];
+};
+
+export type EducationAchievement = {
+  title: string;
   institution: string;
   startDate: string;
   endDate: string;
@@ -17,24 +46,15 @@ export type Education = {
 export type OnlineCourse = {
   course: string;
   platform: string;
-};
-
-export type Languages = {
-  french: string;
-  english: string;
-};
-
-export type Contact = {
-  email: string;
-  phone: string;
-  address: string;
+  credentialId: string;
+  issueDate: string;
 };
 
 export type CvData = {
-  professionalExperience: ProfessionalExperience[];
-  skills: Skill[];
-  education: Education[];
-  onlineCourses: OnlineCourse[];
-  languages: Languages;
   contact: Contact;
+  languages: Language[];
+  skills: Skill[];
+  companies: Company[];
+  educationAchievements: EducationAchievement[];
+  onlineCourses: OnlineCourse[];
 };
