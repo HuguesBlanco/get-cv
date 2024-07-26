2fc0ee3147ba92f943d3b0034b43614f1164e10f
Hugues Blanco <contact@huguesblanco.com>
2024-07-26 13:28:08 +0200

    Improve CvHeader component
    - Added profile image to the component.
    - Created the layout of the component.
    - Styled the component.

--- src/components/CvHeader.tsx
+++ src/components/CvHeader.tsx
@@ -1,21 +1,59 @@
-import { Text, View } from '@react-pdf/renderer';
+import { Image, Text, View } from '@react-pdf/renderer';
 import { CvData } from '../services/cvServiceTypes';
+import CvSectionContainer from './CvSectionContainer';
 
 type CvHeaderProps = { cvData: CvData };
 
 function CvHeader({ cvData }: CvHeaderProps): JSX.Element {
+  const COLOR = '#a3c3e4';
+  const IMAGE_SIZE = '35mm';
+
   return (
-    <View>
-      <View>
-        <Text>{cvData.name.firstName}</Text>
+    <CvSectionContainer>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+        <View>
+          <Text style={{ fontSize: '21pt', letterSpacing: '5' }}>
+            {cvData.name.firstName}
+          </Text>
+          <Text
+            style={{
+              color: COLOR,
+              fontSize: '42pt',
+              letterSpacing: '2',
+              margin: '-2pt',
+              textTransform: 'uppercase',
+            }}
+          >
+            {cvData.name.lastName}
+          </Text>
+          <Text
+            style={{ fontSize: '11pt', letterSpacing: '1', marginLeft: '59mm' }}
+          >
+            {cvData.objective}
+          </Text>
+        </View>
+        <View>
+          <View
+            style={{
+              backgroundColor: COLOR,
+              width: IMAGE_SIZE,
+              height: IMAGE_SIZE,
+              borderRadius: '50%',
+            }}
+          >
+            <Image
+              src={'src/assets/photo-cv.jpg'}
+              style={{
+                width: IMAGE_SIZE,
+                height: IMAGE_SIZE,
+                borderRadius: '50%',
+                opacity: 0.9,
+              }}
+            />
+          </View>
+        </View>
       </View>
-      <View>
-        <Text>{cvData.name.lastName}</Text>
-      </View>
-      <View>
-        <Text>{cvData.objective}</Text>
-      </View>
-    </View>
+    </CvSectionContainer>
   );
 }
 
