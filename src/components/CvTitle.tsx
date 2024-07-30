1e42675ad289ef3b990e2fcd891686e74ef6f0bc
Hugues Blanco <contact@huguesblanco.com>
2024-07-30 16:24:54 +0200

    Remove unnecessary feature and enhance functionality
    - Simplified <CvTitle> component by restricting it to accept only text as children.
    - Added automatic transformation of title text to uppercase for consistency.

--- src/components/CvTitle.tsx
+++ src/components/CvTitle.tsx
@@ -1,19 +1,15 @@
 import { Text, View } from '@react-pdf/renderer';
-import { ReactElement } from 'react';
-import { isString } from '../utils/typeChecks';
 
 type CvTitleProps = {
-  children:
-    | string
-    | ReactElement<typeof View>
-    | ReactElement<typeof Text>
-    | (ReactElement<typeof View> | ReactElement<typeof Text>)[];
+  children: string;
 };
 
 function CvTitle({ children }: CvTitleProps): JSX.Element {
+  const titleText = children.toUpperCase();
+
   return (
     <View style={{ paddingBottom: '10', color: '#a3c3e4', fontSize: '20' }}>
-      {isString(children) ? <Text>{children}</Text> : children}
+      <Text>{titleText}</Text>
     </View>
   );
 }
