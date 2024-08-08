1e25df8e99c91a7a5190857fe1dd832aa7ec316a
Hugues Blanco <contact@huguesblanco.com>
2024-08-08 15:22:56 +0200

    Styled header

--- src/components/sections/CvHeader.tsx
+++ src/components/sections/CvHeader.tsx
@@ -5,13 +5,20 @@ import { PdfViewElement } from '../types';
 type CvHeaderProps = { cv: Cv; color: string };
 
 function CvHeader({ cv, color }: CvHeaderProps): PdfViewElement {
-  const IMAGE_SIZE = '35mm';
+  const IMAGE_SIZE = '33mm';
 
   return (
     <View>
       <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
         <View>
-          <Text style={{ fontSize: '18pt', letterSpacing: '10' }}>
+          <Text
+            style={{
+              fontSize: '20pt',
+              letterSpacing: '9',
+              lineHeight: 1.1, // 1 = 100%, cf. https://github.com/diegomura/react-pdf/issues/2369
+              textTransform: 'uppercase',
+            }}
+          >
             {cv.name.firstName}
           </Text>
           <Text
@@ -19,15 +26,19 @@ function CvHeader({ cv, color }: CvHeaderProps): PdfViewElement {
               color: color,
               fontSize: '40pt',
               fontWeight: 'light',
-              letterSpacing: '5',
-              margin: '-2pt',
+              letterSpacing: '2.5',
+              marginLeft: '-2',
               textTransform: 'uppercase',
             }}
           >
             {cv.name.lastName}
           </Text>
           <Text
-            style={{ fontSize: '11pt', letterSpacing: '1', marginLeft: '63mm' }}
+            style={{
+              fontSize: '11pt',
+              letterSpacing: '1',
+              marginLeft: '56.5mm',
+            }}
           >
             {cv.objective}
           </Text>
@@ -47,7 +58,7 @@ function CvHeader({ cv, color }: CvHeaderProps): PdfViewElement {
                 width: IMAGE_SIZE,
                 height: IMAGE_SIZE,
                 borderRadius: '50%',
-                opacity: 0.9,
+                opacity: 0.8,
               }}
             />
           </View>
