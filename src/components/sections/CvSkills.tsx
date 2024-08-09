a0d2a826299ad19c4e5a332fd1e3c5e6ced9c9a8
Hugues Blanco <contact@huguesblanco.com>
2024-08-09 15:45:55 +0200

    Combine "JavaScript" and "TypeScript" skills

--- src/components/sections/CvSkills.tsx
+++ src/components/sections/CvSkills.tsx
@@ -5,14 +5,13 @@ import CvTitle1 from '../primitives/CvTitle1';
 import { PdfViewElement } from '../types';
 
 /**
- * Filters out a predefined set of skills from the original list to make it fit in the CV.
+ * Filters out a predefined set of skills from the original list to make it fit in the CV.\
  * This allows the full list to be used elsewhere, such as on a website.
  * @param allSkills The complete list of skills from the original data.
  * @returns A list of skills excluding the predefined set of skills to remove.
  */
 function filterSkillsForCv(allSkills: Skill[]): Skill[] {
-  const SKILLS_TO_REMOVE: Skill[] = [
-    'JavaScript',
+  const skillsToRemove: string[] = [
     'Scrum',
     'Adaptability',
     'Data Marketing Strategy',
@@ -29,36 +28,40 @@ function filterSkillsForCv(allSkills: Skill[]): Skill[] {
     'Layout Design',
   ];
 
-  const allSkillsNormalized = SKILLS_TO_REMOVE.map((skill) =>
+  const normalizedSkillsToRemove = skillsToRemove.map((skill) =>
     skill.toLowerCase(),
   );
 
   return allSkills.filter((skill) => {
-    const skillNormalized = skill.toLowerCase();
-
-    const isSkillToRemove = allSkillsNormalized.includes(skillNormalized);
-
-    if (isSkillToRemove) return false;
-    return true;
+    const normalizedSkill = skill.toLowerCase();
+    return !normalizedSkillsToRemove.includes(normalizedSkill);
   });
 }
 
 /**
- * Combines "HTML" and "CSS" into a single entry "HTML/CSS" in the list of skills.
+ * Combines "HTML" and "CSS" into a single entry "HTML/CSS" in the list of skills.\
+ * Also combines "JavaScript" and "TypeScript" into "JavaScript/TypeScript".\
  * This helps to save space in the CV while keeping the data separate in the source.
  * @param skills The list of skills.
- * @returns The list of skills with "HTML" and "CSS" combined into "HTML/CSS".
+ * @returns The list of skills with combined entries.
  */
+function combineSkills(skills: Skill[]): Skill[] {
+  const filteredSkills = skills.filter(
+    (skill) => !/^(html|css|javascript|typescript)$/i.test(skill),
+  );
 
-function combineHtmlAndCss(skills: Skill[]): Skill[] {
-  const filteredSkills = skills.filter((skill) => !/^(html|css)$/i.test(skill));
   const hasHtmlOrCss = skills.some((skill) => /^(html|css)$/i.test(skill));
+  const hasJavaScriptOrTypeScript = skills.some((skill) =>
+    /^(javascript|typescript)$/i.test(skill),
+  );
 
-  if (hasHtmlOrCss) {
-    return [...filteredSkills, 'HTML/CSS'];
-  }
+  const combinedSkills = [
+    ...(hasJavaScriptOrTypeScript ? ['JavaScript/TypeScript'] : []),
+    ...(hasHtmlOrCss ? ['HTML/CSS'] : []),
+    ...filteredSkills,
+  ];
 
-  return filteredSkills;
+  return combinedSkills;
 }
 
 type CvSkillsProps = {
@@ -67,13 +70,13 @@ type CvSkillsProps = {
 };
 
 function CvSkills({ skills, color }: CvSkillsProps): PdfViewElement {
-  const skillsWithHtmlAndCssCombined = combineHtmlAndCss(skills);
-  const reducedSkills = filterSkillsForCv(skillsWithHtmlAndCssCombined);
+  const combinedSkills = combineSkills(skills);
+  const filteredSkillsForCv = filterSkillsForCv(combinedSkills);
 
   return (
     <View>
       <CvTitle1 color={color}>Skills</CvTitle1>
-      {reducedSkills.map((skill) => (
+      {filteredSkillsForCv.map((skill) => (
         <CvListItem key={skill} icon="circle" isBottomSpacingEnabled={false}>
           {skill}
         </CvListItem>
