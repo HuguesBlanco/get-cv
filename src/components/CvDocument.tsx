5f17a25f09eda1ce559c9e8a4af5cefda8ffdb9e
Hugues Blanco <contact@huguesblanco.com>
2024-09-13 23:10:23 +0200

    Add CvResume component for CV page
    - Created `CvResume` component to enhance clarity and improve structure.

--- src/components/CvDocument.tsx
+++ src/components/CvDocument.tsx
@@ -1,17 +1,8 @@
 import { Document, Font, Text } from '@react-pdf/renderer';
 import { getCv } from '../services/cvService';
 import { Languages } from '../types';
+import CvResume from './pages/CvResume';
 import CvPage from './primitives/CvPage';
-import CvPart from './primitives/CvPart';
-import CvSlot from './primitives/CvSlot';
-import CvContact from './sections/CvContact';
-import CvEducation from './sections/CvEducation';
-import CvGithub from './sections/CvGithub';
-import CvHeader from './sections/CvHeader';
-import CvLanguages from './sections/CvLanguages';
-import CvOnlineCourses from './sections/CvOnlineCourses';
-import CvSkills from './sections/CvSkills';
-import CvWorkExperience from './sections/CvWorkExperience';
 import { PdfDocumentElement } from './types';
 
 type CvDocumentProps = {
@@ -33,50 +24,7 @@ function CvDocument({
 
   return (
     <Document>
-      {isCvIncluded && (
-        <CvPage>
-          <CvPart>
-            <CvSlot>
-              <CvHeader cv={cv} color={COLOR} />
-            </CvSlot>
-          </CvPart>
-
-          <CvPart layoutDirection="horizontal">
-            <CvSlot
-              widthPercentage={33.333}
-              isBorderRightVisible={true}
-              borderColor={COLOR}
-            >
-              <CvContact contact={cv.contact} color={COLOR} />
-              <CvSkills skills={cv.skills} color={COLOR} language={language} />
-              <CvLanguages languages={cv.languages} color={COLOR} />
-            </CvSlot>
-
-            <CvSlot widthPercentage={66.666}>
-              <CvWorkExperience companies={cv.companies} color={COLOR} />
-            </CvSlot>
-          </CvPart>
-
-          <CvPart layoutDirection="horizontal">
-            <CvSlot widthPercentage={33.333}>
-              <CvOnlineCourses onlineCourses={cv.onlineCourses} color={COLOR} />
-            </CvSlot>
-
-            <CvSlot widthPercentage={66.666}>
-              <CvEducation
-                educationAchievements={cv.educationAchievements}
-                color={COLOR}
-              />
-            </CvSlot>
-          </CvPart>
-
-          <CvPart>
-            <CvSlot>
-              <CvGithub language={language} color={COLOR} />
-            </CvSlot>
-          </CvPart>
-        </CvPage>
-      )}
+      {isCvIncluded && <CvResume cv={cv} language={language} color={COLOR} />}
       {isCoverLetterIncluded && (
         <CvPage>
           <Text>Cover letter</Text>
---
+++ src/components/pages/CvResume.tsx
@@ -0,0 +1,69 @@
+import { Cv } from '../../services/cvServiceTypes';
+import { Languages } from '../../types';
+import CvPage from '../primitives/CvPage';
+import CvPart from '../primitives/CvPart';
+import CvSlot from '../primitives/CvSlot';
+import CvContact from '../sections/CvContact';
+import CvEducation from '../sections/CvEducation';
+import CvGithub from '../sections/CvGithub';
+import CvHeader from '../sections/CvHeader';
+import CvLanguages from '../sections/CvLanguages';
+import CvOnlineCourses from '../sections/CvOnlineCourses';
+import CvSkills from '../sections/CvSkills';
+import CvWorkExperience from '../sections/CvWorkExperience';
+import { PdfPageElement } from '../types';
+
+type CvResumeProps = {
+  cv: Cv;
+  language: Languages;
+  color: string;
+};
+
+function CvResume({ cv, language, color }: CvResumeProps): PdfPageElement {
+  return (
+    <CvPage>
+      <CvPart>
+        <CvSlot>
+          <CvHeader cv={cv} color={color} />
+        </CvSlot>
+      </CvPart>
+
+      <CvPart layoutDirection="horizontal">
+        <CvSlot
+          widthPercentage={33.333}
+          isBorderRightVisible={true}
+          borderColor={color}
+        >
+          <CvContact contact={cv.contact} color={color} />
+          <CvSkills skills={cv.skills} color={color} language={language} />
+          <CvLanguages languages={cv.languages} color={color} />
+        </CvSlot>
+
+        <CvSlot widthPercentage={66.666}>
+          <CvWorkExperience companies={cv.companies} color={color} />
+        </CvSlot>
+      </CvPart>
+
+      <CvPart layoutDirection="horizontal">
+        <CvSlot widthPercentage={33.333}>
+          <CvOnlineCourses onlineCourses={cv.onlineCourses} color={color} />
+        </CvSlot>
+
+        <CvSlot widthPercentage={66.666}>
+          <CvEducation
+            educationAchievements={cv.educationAchievements}
+            color={color}
+          />
+        </CvSlot>
+      </CvPart>
+
+      <CvPart>
+        <CvSlot>
+          <CvGithub language={language} color={color} />
+        </CvSlot>
+      </CvPart>
+    </CvPage>
+  );
+}
+
+export default CvResume;
