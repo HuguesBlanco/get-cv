37bd4a38127e0836e8f30437ae19c1fa6a25114b
Hugues Blanco <contact@huguesblanco.com>
2024-09-13 23:31:45 +0200

    Add structure in cover letter

--- src/components/pages/CvCoverLetter.tsx
+++ src/components/pages/CvCoverLetter.tsx
@@ -1,11 +1,17 @@
 import { Text } from '@react-pdf/renderer';
 import CvPage from '../primitives/CvPage';
+import CvPart from '../primitives/CvPart';
+import CvSlot from '../primitives/CvSlot';
 import { PdfPageElement } from '../types';
 
 function CvCoverLetter(): PdfPageElement {
   return (
     <CvPage>
-      <Text>Cover letter</Text>
+      <CvPart>
+        <CvSlot>
+          <Text>Cover letter</Text>
+        </CvSlot>
+      </CvPart>
     </CvPage>
   );
 }
