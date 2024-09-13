85573c8f91a359a1526d66c73097d36a13d73520
Hugues Blanco <contact@huguesblanco.com>
2024-09-13 22:28:16 +0200

    Add reusable `CvSlot` component for layout consistency
    - Created reusable `CvSlot` component to streamline layout structure.
    - Centralized padding styling within the component.
    - Removed unused ID attribute from slots.

--- src/components/CvDocument.tsx
+++ src/components/CvDocument.tsx
@@ -1,8 +1,9 @@
-import { Document, Font, Text, View } from '@react-pdf/renderer';
+import { Document, Font, Text } from '@react-pdf/renderer';
 import { getCv } from '../services/cvService';
 import { Languages } from '../types';
 import CvPage from './primitives/CvPage';
 import CvPart from './primitives/CvPart';
+import CvSlot from './primitives/CvSlot';
 import CvContact from './sections/CvContact';
 import CvEducation from './sections/CvEducation';
 import CvGithub from './sections/CvGithub';
@@ -28,7 +29,6 @@ function CvDocument({
 
   Font.registerHyphenationCallback((word) => [word]);
 
-  const SLOTS_MARGIN_X = '5mm';
   const COLOR = '#4b6f96';
 
   return (
@@ -36,84 +36,44 @@ function CvDocument({
       {isCvIncluded && (
         <CvPage>
           <CvPart>
-            <View
-              id="slot-1"
-              style={{
-                marginLeft: SLOTS_MARGIN_X,
-                marginRight: SLOTS_MARGIN_X,
-              }}
-            >
+            <CvSlot>
               <CvHeader cv={cv} color={COLOR} />
-            </View>
+            </CvSlot>
           </CvPart>
 
           <CvPart layoutDirection="horizontal">
-            <View
-              id="slot-2"
-              style={{
-                borderRight: `0.4mm solid ${COLOR}`,
-                marginLeft: SLOTS_MARGIN_X,
-                marginRight: SLOTS_MARGIN_X,
-                width: '33.333%',
-                justifyContent: 'space-between',
-              }}
+            <CvSlot
+              widthPercentage={33.333}
+              isBorderRightVisible={true}
+              borderColor={COLOR}
             >
               <CvContact contact={cv.contact} color={COLOR} />
-
               <CvSkills skills={cv.skills} color={COLOR} language={language} />
-
               <CvLanguages languages={cv.languages} color={COLOR} />
-            </View>
+            </CvSlot>
 
-            <View
-              id="slot-3"
-              style={{
-                marginLeft: SLOTS_MARGIN_X,
-                marginRight: SLOTS_MARGIN_X,
-                width: '66.666%',
-              }}
-            >
+            <CvSlot widthPercentage={66.666}>
               <CvWorkExperience companies={cv.companies} color={COLOR} />
-            </View>
+            </CvSlot>
           </CvPart>
 
           <CvPart layoutDirection="horizontal">
-            <View
-              id="slot-4"
-              style={{
-                marginLeft: SLOTS_MARGIN_X,
-                marginRight: SLOTS_MARGIN_X,
-                width: '33.333%',
-              }}
-            >
+            <CvSlot widthPercentage={33.333}>
               <CvOnlineCourses onlineCourses={cv.onlineCourses} color={COLOR} />
-            </View>
+            </CvSlot>
 
-            <View
-              id="slot-5"
-              style={{
-                marginLeft: SLOTS_MARGIN_X,
-                marginRight: SLOTS_MARGIN_X,
-                width: '66.666%',
-              }}
-            >
+            <CvSlot widthPercentage={66.666}>
               <CvEducation
                 educationAchievements={cv.educationAchievements}
                 color={COLOR}
               />
-            </View>
+            </CvSlot>
           </CvPart>
 
           <CvPart>
-            <View
-              id="slot-6"
-              style={{
-                marginLeft: SLOTS_MARGIN_X,
-                marginRight: SLOTS_MARGIN_X,
-              }}
-            >
+            <CvSlot>
               <CvGithub language={language} color={COLOR} />
-            </View>
+            </CvSlot>
           </CvPart>
         </CvPage>
       )}
