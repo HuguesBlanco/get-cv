512c4b4f71bfbbb9699c1569eed582c4c93a0751
Hugues Blanco <contact@huguesblanco.com>
2024-11-07 16:20:23 +0100

    Fix inline bold and italic styling in cover letter text
    Resolved an issue where inline bold and italic styling was only applied if set to the entire segments, due to a bug in react-pdf. Implemented a workaround to support inline styling:
    - Created a specific components for handling inline bold and italic text, using separate fonts for each style.
    - Downgraded `@react-pdf/layout` to version 3.11.5 to bypass regression preventing workaround in higher versions.
    - Downgraded `@react-pdf/renderer` to version 3.4.2 to support `@react-pdf/layout` version 3.11.5 as a dependency.

--- src/cv/elements/CvTextSections.tsx
+++ src/cv/elements/CvTextSections.tsx
@@ -1,7 +1,91 @@
-import { Text, View } from '@react-pdf/renderer';
+import { Font, Text, View } from '@react-pdf/renderer';
+import sourceSansBoldURL from '../assets/fonts/SourceSans3-Bold.ttf';
+import sourceSansItalicURL from '../assets/fonts/SourceSans3-Italic.ttf';
 import CvListItem from '../primitives/CvListItem';
 import { Paragraph, SegmentType } from '../types/cvTypes';
 import { PdfTextElement, PdfViewElement } from '../types/pdfTypes';
+type CvInlineBoldProps = {
+  children: string;
+};
+
+/**
+ * Renders inline text in bold within a PDF document.
+ *
+ * This component exists as a workaround for an issue in `@react-pdf/renderer` that prevents inline bold text from displaying correctly.\
+ * See {@link https://github.com/diegomura/react-pdf/issues/164} for more information.
+ *
+ * **Note:** For compatibility, use `@react-pdf/layout` version 3.11.5 or lower alongside `@react-pdf/renderer` version 3.4.2.\
+ * See {@link https://github.com/diegomura/react-pdf/issues/2855} for related issues.
+ *
+ * @param props See {@link CvInlineBoldProps}.
+ * @returns A <Text> element rendered in bold.
+ */
+function CvInlineBold({ children }: CvInlineBoldProps): PdfTextElement {
+  const BOLD_FONT = 'Source-Sans-3-bold';
+
+  const isFontRegistred = Font.getRegisteredFontFamilies().includes(BOLD_FONT);
+
+  if (!isFontRegistred) {
+    Font.register({
+      family: BOLD_FONT,
+      fonts: [
+        {
+          src: sourceSansBoldURL,
+          fontStyle: 'normal',
+          fontWeight: 'normal',
+        },
+      ],
+    });
+  }
+
+  return (
+    <Text debug style={{ fontFamily: BOLD_FONT }}>
+      {children}
+    </Text>
+  );
+}
+
+type CvInlineItalicProps = {
+  children: string;
+};
+
+/**
+ * Renders inline text in italic within a PDF document.
+ *
+ * This component exists as a workaround for an issue in `@react-pdf/renderer` that prevents inline italic text from displaying correctly.\
+ * See {@link https://github.com/diegomura/react-pdf/issues/164} for more information.
+ *
+ * **Note:** For compatibility, use `@react-pdf/layout` version 3.11.5 or lower alongside `@react-pdf/renderer` version 3.4.2.\
+ * See {@link https://github.com/diegomura/react-pdf/issues/2855} for related issues.
+ *
+ * @param props See {@link CvInlineItalicProps}.
+ * @returns A <Text> element rendered in italic.
+ */
+function CvInlineItalic({ children }: CvInlineItalicProps): PdfTextElement {
+  const ITALIC_FONT = 'Source-Sans-3-italic';
+
+  const isFontRegistred =
+    Font.getRegisteredFontFamilies().includes(ITALIC_FONT);
+
+  if (!isFontRegistred) {
+    Font.register({
+      family: ITALIC_FONT,
+      fonts: [
+        {
+          src: sourceSansItalicURL,
+          fontStyle: 'normal',
+          fontWeight: 'normal',
+        },
+      ],
+    });
+  }
+
+  return (
+    <Text debug style={{ fontFamily: ITALIC_FONT }}>
+      {children}
+    </Text>
+  );
+}
 
 function parseStylesInText(text: string): PdfTextElement {
   const boldPattern = /\*\*(?:.*?)\*\*/;
@@ -19,21 +103,13 @@ function parseStylesInText(text: string): PdfTextElement {
       const isBoldText = boldPattern.test(textSegment);
       if (isBoldText) {
         const boldText = textSegment.slice(2, -2);
-        return (
-          <Text debug key={segmentIndex} style={{ fontWeight: 'bold' }}>
-            {boldText}
-          </Text>
-        );
+        return <CvInlineBold key={segmentIndex}>{boldText}</CvInlineBold>;
       }
 
       const isItalicText = italicPattern.test(textSegment);
       if (isItalicText) {
         const italicText = textSegment.slice(1, -1);
-        return (
-          <Text key={segmentIndex} style={{ fontStyle: 'italic' }}>
-            {italicText}
-          </Text>
-        );
+        return <CvInlineItalic key={segmentIndex}>{italicText}</CvInlineItalic>;
       }
 
       return textSegment;
